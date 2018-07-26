080fbb983fadca9ef498cd8f6d1b733dcaa99da3
Jiadong.Yu <jiadong@pandu.ie>
2018-07-26 17:02:51 +0800

    first commit

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'max-empty-lines': 1,
+    'declaration-colon-newline-after': null,
+    'value-list-comma-newline-after': null,
+    'block-no-empty': null,
+    'at-rule-no-unknown': null,
+  },
+}
